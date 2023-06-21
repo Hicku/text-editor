c8fcf71f1d7dff20422a921dc4c07d737fc5239f
Quas <aaron.hickman201@gmail.com>
2023-06-21 16:13:45 +0100

    database functions

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ id: 1, content: content });
+    const result = await request;
+  } catch (error) {
+    console.error('putDb error:', error);
+  }
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+
+    if (result.length < 1) {
+      return;
+    }
+
+    return result[0].content;
+  } catch (error) {
+    console.error('getDb error:', error);
+  }
+};
 
 initdb();
